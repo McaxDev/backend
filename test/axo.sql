a72f19b24d558af6bfe637653a8634a1dac033e9
bestcb2333 <2892709432@qq.com>
2025-01-13 23:53:32 +0800

    1/13

--- test/axo.sql
+++
@@ -1,3 +0,0 @@
-
--- users
-insert into users ()
--- test/post.sh
+++
@@ -1,4 +0,0 @@
-http GET https://api.axtl.cn:520/account/get/userinfo \
-Authorization:'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzY2MjYyODIsInVzZXJJZCI6MX0.d4WzyQLToU54wqorvmkNXB84BcDzhBsEVS28G4bdXHA' \
-Content-Type:application/json \
-'1'
